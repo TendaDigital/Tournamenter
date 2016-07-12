bf247d36ab838bc299d8564f91295f63ea497621
Ivan Seidel <ivanseidel@gmail.com>
2016-07-12 12:58:28 -0300

    Add APP_UID config in ENV vars

--- config/env/development.js
+++ config/env/development.js
@@ -1,11 +1,13 @@
 var env = process.env;
 var port = process.env.PORT || 3000;
 var domain = 'http://localhost:'+port;
+var appUid = env.APP_UID || env.APP_NAME || 'tournamenter';
 
 module.exports = {
 	port: port,
 	domain: domain,
 
+  appUid: appUid,
   appName: env.APP_NAME || require('../../package.json').name,
   version: env.APP_NAME || require('../../package.json').version,
   password: env.PASSWORD || null,
@@ -17,7 +19,7 @@ module.exports = {
 	adapter: env.DB_ADAPTER || 'sails-disk',
 
   connection: {
-    filePath: env.DB_FILEPATH || './.tmp/default/',
+    filePath: env.DB_FILEPATH || './.tmp/'+appUid,
     url: env.DB_URL || null,
   },
 
--- config/env/production.js
+++ config/env/production.js
@@ -1,15 +1,26 @@
 var env = process.env;
 var port = process.env.PORT || 8000;
 var domain = 'http://yourwebsite.com.br';
+var appUid = env.APP_UID || env.APP_NAME || 'tournamenter';
 
 module.exports = {
-	port: port,
+  port: port,
 	domain: domain,
+
+  appUid: appUid,
+  appName: env.APP_NAME || require('../../package.json').name,
+  version: env.APP_NAME || require('../../package.json').version,
+  password: env.PASSWORD || null,
 
 	session: {
 		secret: 'somesecretstring',
 	},
 
-	db: env.MONGODB_URL || 'mongodb://yourmongodburlhere:port/...',
+	adapter: env.DB_ADAPTER || 'sails-disk',
+
+  connection: {
+    filePath: env.DB_FILEPATH || './.tmp/'+appUid,
+    url: env.DB_URL || null,
+  },
 
 };
