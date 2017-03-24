14746fb3c3115f5cd9b49072cb334dc49e61dac7
Ivan Seidel <ivanseidel@gmail.com>
2017-03-24 22:31:41 +0100

    Show server configs

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
 /**
  * Module dependencies.
  */
+var chalk = require('chalk');
+var TAG = chalk.white('[CONFIG]');
 
 var path = require('path');
 var extend = require('util')._extend;
@@ -18,3 +20,34 @@ var defaults = {
  */
 
 module.exports = extend(enviroment, defaults);
+
+/**
+ * Log Config information
+ */
+console.log();
+console.log();
+console.log(TAG, chalk.cyan('             SERVER'));
+console.log(TAG, chalk.blue('       root:'), module.exports.root);
+console.log(TAG, chalk.blue('       port:'), module.exports.port);
+console.log();
+console.log(TAG, chalk.cyan('             TOURNAMENTER'));
+console.log(TAG, chalk.blue('    version:'), module.exports.version);
+console.log();
+console.log(TAG, chalk.cyan('             SECURITY'));
+console.log(TAG, chalk.blue('   password:'), module.exports.password || '<no authentication>');
+console.log();
+console.log(TAG, chalk.cyan('              APP'));
+console.log(TAG, chalk.blue('        env:'), module.exports.env);
+console.log(TAG, chalk.blue('     appUid:'), module.exports.appUid);
+console.log(TAG, chalk.blue('    appLogo:'), module.exports.appLogo);
+console.log(TAG, chalk.blue('    appName:'), module.exports.appName);
+console.log();
+console.log(TAG, chalk.cyan('             DATABASE'));
+console.log(TAG, chalk.blue('        url:'), module.exports.connection.url || '<no url>');
+console.log(TAG, chalk.blue('    adapter:'), module.exports.adapter);
+console.log(TAG, chalk.blue('   filePath:'), module.exports.connection.filePath);
+console.log();
+console.log(TAG, chalk.cyan('             EXTENSIONS [TOURNAMENTER_EXTENSIONS]'));
+console.log(TAG, chalk.blue(' extensions:'), process.env.TOURNAMENTER_EXTENSIONS || '<no extensions>');
+console.log();
+console.log();
